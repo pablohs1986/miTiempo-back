6ec517510e2ee2e51fda59a8f00deb6d5dc99313
pablohs1986 <pabloherrero1986@gmail.com>
2021-04-15 13:10:20 +0200

    fixing getUserInfo rout.

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,17 +6,19 @@ const requireAuth = require('../middlewares/requireAuth');
 
 // Express router instance
 const router = express.Router();
+router.use(requireAuth); //Everything done here, need to validate the token
 
 /** Method that retrieves the information of a user.
  * It receives a token that validates. If the validation is successful,
  * send the user information. If the validation is not correct,
  * it sends an error message.
  */
-router.get('/getUserInfo', requireAuth, async (req, res) => {
-	const user = await User.findOne(req.user._id);
+router.get('/getUserInfo', async (req, res) => {
+	const user = await User.findById(req.user._id);
 
 	try {
 		res.send({ user });
+		console.log(user);
 	} catch (error) {
 		return res.status(422).send({
 			Error:
