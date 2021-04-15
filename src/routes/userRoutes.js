8bcc3a94716436dda63912b355f6453a69ebd5f3
pablohs1986 <pabloherrero1986@gmail.com>
2021-04-15 10:57:22 +0200

    getUserInfo route refactored, clened and documented.

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,16 +7,20 @@ const requireAuth = require('../middlewares/requireAuth');
 // Express router instance
 const router = express.Router();
 
-/** TODO: documentar */
+/** Method that retrieves the information of a user.
+ * It receives a token that validates. If the validation is successful,
+ * send the user information. If the validation is not correct,
+ * it sends an error message.
+ */
 router.get('/getUserInfo', requireAuth, async (req, res) => {
-	const email = await req.user.email;
-	const user = await User.findOne({ email });
+	const user = await User.findOne(req.user._id);
 
 	try {
 		res.send({ user });
 	} catch (error) {
 		return res.status(422).send({
-			Error: 'Something wrong when accesing to database. Try again.',
+			Error:
+				'Something went wrong retrieving user information. Try again.',
 		});
 	}
 });
